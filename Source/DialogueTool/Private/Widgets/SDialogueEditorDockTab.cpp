3fdd4707beb08075a349cf5542797a593e4fe3fe
akete <411966099@qq.com>
2024-04-15 11:35:01 +0800

    传入的FWorkBook参数由从Construct直接传入改为 从FArguments  传入

--- Source/DialogueTool/Private/Widgets/SDialogueEditorDockTab.cpp
+++ Source/DialogueTool/Private/Widgets/SDialogueEditorDockTab.cpp
@@ -13,11 +13,9 @@ void SDialogueEditorDockTab::Construct(const FArguments& InArgs)
 	SDockTab::Construct(SDockTab::FArguments()
 		.TabRole(ETabRole::NomadTab));
 
-	FText WidgetText = FText::Format(
-		LOCTEXT("WindowWidgetText", "Add code in {0} to override this window's contents"),
-		FText::FromString(TEXT("SDialogueEditorDockTab.cpp")));
-
-	TSharedRef<SWidget> NewContent = SNew(SDialogueToolGraphWindow, FWorkBook());
+	TSharedRef<SWidget> NewContent = 
+		SNew(SDialogueToolGraphWindow)
+		.WorkBook(FWorkBook());
 
 	SetContent(NewContent);
 }
